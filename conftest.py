745277af45bbdd64a07a4db36b50d1e7a54a44b1
Shakerov Ravil <xellaopromaster@yandex.ru>
2020-10-15 22:18:46 +0300

    Edited project
    Added a lot of dirs to git igore. Added init, reqs, and simple test case.

--- conftest.py
+++ conftest.py
@@ -1,10 +1,31 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='ru',
+                     help="Choose language")
+
 
 @pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
+def browser(request):
+    browser_language, browser, languages = request.config.getoption("language"), None, ['ar', 'ca', 'cs', 'da', 'de',
+                                                                                        'en-gb', 'el', 'es', 'fi', 'fr',
+                                                                                        'it', 'ko', 'nl', 'pl', 'pt',
+                                                                                        'pt-br', 'ro', 'ru', 'sk', 'uk',
+                                                                                        'zh-hans', 'en']
+    for language in languages:
+        if browser_language == language:
+            print(f"\n{browser_language} language")
+            options = Options()
+            options.add_experimental_option('prefs', {'intl.accept_languages': browser_language})
+            browser = webdriver.Chrome(options=options)
+            break
+        elif languages.index(language) + 1 == len(languages) and browser_language != language:
+            raise pytest.UsageError("--language should be shortcut of language")
+        else:
+            continue
     yield browser
     print("\nquit browser..")
     browser.quit()
