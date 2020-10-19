0617c3e4f4e267ce51f6c50e2f4167e6aba5d03d
Ravil Shakerov <xellaopromaster@yandex.ru>
2020-10-19 22:09:06 +0300

    Added new class ProductPage, new locators, and test case

--- pages/locators.py
+++ pages/locators.py
@@ -7,7 +7,6 @@ class MainPageLocators():
 
 
 class LoginPageLocators():
-
     # Sing in\ Login
     LOGIN_FORM = (By.CSS_SELECTOR, 'form#login_form')
     EMAIL_SING_IN = (By.CSS_SELECTOR, 'input#id_login-username.form-control')
@@ -20,3 +19,9 @@ class LoginPageLocators():
     PASS_SING_UP_1 = (By.CSS_SELECTOR, 'input#id_registration-password1.form-control')
     PASS_SING_UP_2 = (By.CSS_SELECTOR, 'input#id_registration-password2.form-control')
     BTN_SING_UP = (By.CSS_SELECTOR, 'form#register_form > button.btn.btn-lg.btn-primary')
+
+
+class ProductPageLocators():
+    BTN_ADD_TO_BASKET = (By.CSS_SELECTOR, 'button.btn.btn-lg.btn-primary.btn-add-to-basket')
+    ITEM_ADDED = (By.CSS_SELECTOR, 'div.alertinner')
+    NAME_OF_BOOK = (By.CSS_SELECTOR, 'div.col-sm-6.product_main > h1')
